462f10187aab9392f6f8264b692cd622c5eb04ab
Krzysztof Koziarski <krzyhook@gmail.com>
2021-01-22 20:53:14 +0100

    added all scaffolded .net Identity: Areas/Identity/**/*.cshtml

---
+++ src/WebUI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CleanArchWeb.Infrastructure.Identity;
+using CleanArchWeb.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CleanArchWeb.WebUI.Areas.Identity.IdentityHostingStartup))]
+namespace CleanArchWeb.WebUI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
