2cad311da8c8b0b19bb575f801e8eecd879a985b
Krzysztof Koziarski <krzyhook@gmail.com>
2021-02-03 21:50:11 +0100

    addec RazorRuntimeCompilation

--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -20,11 +20,13 @@ namespace CleanArchWeb.WebUI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Env = env;
         }
 
+        public IWebHostEnvironment Env { get; set; }
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -47,6 +49,15 @@ namespace CleanArchWeb.WebUI
                     .AddFluentValidation();
 
             services.AddRazorPages();
+
+            IMvcBuilder builder = services.AddRazorPages();
+
+#if DEBUG
+            if (Env.IsDevelopment())
+            {
+                builder.AddRazorRuntimeCompilation();
+            }
+#endif
 
             // Customise default API behaviour
             services.Configure<ApiBehaviorOptions>(options =>
