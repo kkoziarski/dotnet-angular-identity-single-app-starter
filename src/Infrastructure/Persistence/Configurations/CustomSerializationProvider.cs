3564972a3b3b553cd6adfcfb03e1b8d415aa9148
Krzysztof Koziarski <krzyhook@gmail.com>
2021-10-14 20:26:53 +0200

    Created Bson CustomSerializationProvider

---
+++ src/Infrastructure/Persistence/Configurations/CustomSerializationProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace CleanArchWeb.Infrastructure.Persistence.Configurations
+{
+    internal class CustomSerializationProvider : IBsonSerializationProvider
+    {
+        public IBsonSerializer GetSerializer(Type type)
+        {
+            return type switch
+            {
+                var _type when _type == typeof(DateTime) => new DateTimeUtcSerializer(),
+                _ => null
+            };
+        }
+    }
+
+    internal class DateTimeUtcSerializer : DateTimeSerializer
+    {
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, DateTime value)
+        {
+            var utcValue = new DateTime(value.Ticks, DateTimeKind.Utc);
+            base.Serialize(context, args, utcValue);
+        }
+    }
+}
